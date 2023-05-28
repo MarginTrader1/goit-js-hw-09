7dd111bb39bcf63ac2b6bee82b58095265ffc7b2
Mykhailo Statsenko <66798324+MarginTrader1@users.noreply.github.com>
2023-05-28 21:59:45 +0300

    сделал второе дз

---
+++ src/js/02-timer.js
@@ -0,0 +1,86 @@
+import flatpickr from "flatpickr";
+import "flatpickr/dist/flatpickr.min.css";
+
+const dateInput = document.querySelector('#datetime-picker');
+const btnStart = document.querySelector('button[data-start]');
+const daysField = document.querySelector('span[data-days]');
+const hoursField = document.querySelector('span[data-hours]');
+const minutesField = document.querySelector('span[data-minutes]');
+const secondsField = document.querySelector('span[data-seconds]');
+
+const options = {
+    enableTime: true,
+    time_24hr: true,
+    defaultDate: new Date(),
+    minuteIncrement: 1,
+    onClose(selectedDates) {
+      if (selectedDates[0] <= new Date()) {
+        btnStart.setAttribute('disabled', '');
+        window.alert("Please choose a date in the future");
+      } else {
+        btnStart.removeAttribute('disabled');
+      }
+    },
+  };
+
+flatpickr(dateInput, options);
+
+btnStart.addEventListener(`click`, startHandleClick);
+
+function startHandleClick(){
+
+  setInterval(() => makeTimer(), 1000);
+
+  function makeTimer() {
+    // текущая дата
+    const currentDate = new Date();
+    
+    // введеная дата
+    const inputDate = Date.parse(dateInput.value);
+
+    // разница между текущей и введеной датой
+    const diffDate = inputDate - currentDate; 
+    
+    // добавляем в код HTML
+    addDateToHtml (convertMs(diffDate));
+  }
+}
+
+// функция конвертации времени - возвращает объект 
+function convertMs(ms) {
+  // Number of milliseconds per unit of time
+  const second = 1000;
+  const minute = second * 60;
+  const hour = minute * 60;
+  const day = hour * 24;
+
+  // Remaining days
+  const days = Math.floor(ms / day);
+  // Remaining hours
+  const hours = Math.floor((ms % day) / hour);
+  // Remaining minutes
+  const minutes = Math.floor(((ms % day) % hour) / minute);
+  // Remaining seconds
+  const seconds = Math.floor((((ms % day) % hour) % minute) / second);
+
+  return { days, hours, minutes, seconds };
+};
+
+// функция добавления времени в код HTML
+function addDateToHtml ({ days, hours, minutes, seconds }) {
+  daysField.textContent = addLeadingZero(days);
+  hoursField.textContent = addLeadingZero(hours);
+  minutesField.textContent = addLeadingZero(minutes);
+  secondsField.textContent = addLeadingZero(seconds);
+}; 
+
+// добавляем ноль спереди цифры
+function addLeadingZero(number){
+  return String(number).padStart(2, 0);
+};
+
+
+
+
+
+
