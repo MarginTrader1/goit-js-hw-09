849d04e327e44fb1de3b24ac8c5e18f72cd9ea5d
Mykhailo Statsenko <66798324+MarginTrader1@users.noreply.github.com>
2023-06-17 21:34:36 +0300

    Update 03-promises.js

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -11,37 +11,43 @@ function clickBtnCreatePromise(event) {
   const step = Number(event.target.elements.step.value);
   const amount = Number(event.target.elements.amount.value);
 
-// функции для успешного выполнения промиса
-const onSuccess = ({ position, delay }) => {
-  Notiflix.Notify.success(`Fulfilled promise ${position} in ${delay}ms`);
-  console.log(`✅ Fulfilled promise ${position} in ${delay}ms`);
-};
+  // функции для успешного выполнения промиса
+  function onSuccess ({ position, delay }) {
+    Notiflix.Notify.success(`Fulfilled promise ${position} in ${delay}ms`);
+    console.log(`✅ Fulfilled promise ${position} in ${delay}ms`);
+  };
 
-// функция для неуспешного выполнения промиса
-const onFailure = ({ position, delay }) => {
-  Notiflix.Notify.failure(`Rejected promise ${position} in ${delay}ms`);
-  console.log(`❌ Rejected promise ${position} in ${delay}ms`);
-};
+  // функция для неуспешного выполнения промиса
+  function onFailure ({ position, delay }) {
+    Notiflix.Notify.failure(`Rejected promise ${position} in ${delay}ms`);
+    console.log(`❌ Rejected promise ${position} in ${delay}ms`);
+  };
 
-// цикл создания промисов
-for (const i of Array(amount).keys()) {
-  createPromise(i + 1, delay + step * i)
-    .then(({ position, delay }) => onSuccess({ position, delay }))
-    .catch(({ position, delay }) => onFailure({ position, delay }));
-}}
+  // функция создания промиса 
+  function createPromise(position, delay) {
 
+    const shouldResolve = Math.random() > 0.3;
 
-// функция createPromise, принимает position - позиция промиса та delay - задержка перед исполнением промиса.
-function createPromise(position, delay) {
-  const shouldResolve = Math.random() > 0.3;
-  console.log(`Creating promise ${position} with delay ${delay}ms`);
-  return new Promise((resolve, reject) => {
-    setTimeout(() => {
-      if (shouldResolve) {
-        resolve({ position, delay });
-      } else {
-        reject({ position, delay });
-      }
-    }, delay);
-  });
+    console.log(`Creating promise ${position} with delay ${delay}ms`);
+
+    return new Promise((resolve, reject) => {
+      setTimeout(() => {
+        if (shouldResolve) {
+          resolve({ position, delay });
+        } else {
+          reject({ position, delay });
+        }
+      }, delay);
+    });
+  }
+
+  for (let i = 0; i < amount; i++) {
+    createPromise(i + 1, delay + step * i)
+    .then(({ position, delay }) => onFailure({ position, delay }))
+    .catch(({ position, delay }) => onFailure({ position, delay }))
+  }
 }
+
+
+
+// for (const i of Array(amount).keys()) - тот же метод, только создаем массив и перебивраем его 
